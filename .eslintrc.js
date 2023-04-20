03ecb8f73ec44f477ded236efb591318b68337f9
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-04-20 16:26:07 +0200

    delete prettier and edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
