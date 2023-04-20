c275d7f1e954db4ddb0405b9c0e49bef2c775635
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-04-20 10:46:51 +0200

    update with correct files

--- build/domains/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.registerRoutes = void 0;
-var registerRoutes = function (app) {
-};
-exports.registerRoutes = registerRoutes;
-//# sourceMappingURL=index.js.map
--- build/utils/radomTokenGenerator.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var randomUUID = require('crypto').randomUUID;
-exports["default"] = (function () { return randomUUID(); });
-//# sourceMappingURL=radomTokenGenerator.js.map
