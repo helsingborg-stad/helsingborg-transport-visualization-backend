ad48e61904af8c468c95330d30ce70319fe96d47
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-04-28 09:43:05 +0200

    update login and signup route

--- src/routes/auth/types.ts
+++ src/routes/auth/types.ts
@@ -1,6 +1,15 @@
 export type LoginBody = {
+  identifier: string;
+  password?: string;
+  pinCode?: string;
+};
+
+export type SignupBody = {
+  id: string;
+  name: string;
   email: string;
   password: string;
+  pinCode: string;
 };
 
 export type ForgotPasswordBody = {
