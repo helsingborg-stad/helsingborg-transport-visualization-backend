0b18a6add7358ccc2d2d6a0efd096aece7d4eacf
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-04-28 09:43:18 +0200

    add validation to API's

--- src/routes/auth/validation/index.ts
+++ src/routes/auth/validation/index.ts
@@ -1,3 +1,4 @@
 export * from './login.validation';
 export * from './forgotPassword.validation';
 export * from './resetPassword.validation';
+export * from './signup.validation';
--- src/routes/auth/validation/signup.validation.ts
+++ src/routes/auth/validation/signup.validation.ts
@@ -1,17 +1,27 @@
 import { validationMiddleware } from '@root/utils/validationMiddleware';
 
-export const loginValidation = validationMiddleware({
-  identifier: {
+export const signupValidation = validationMiddleware({
+  id: {
     presence: true,
     type: 'string',
   },
+  name: {
+    presence: true,
+    type: 'string',
+  },
+  email: {
+    presence: true,
+    email: true,
+  },
   password: {
+    presence: true,
     length: {
       minimum: 10,
       message: 'must have at least 10 characters.',
     },
   },
   pinCode: {
+    presence: true,
     length: {
       minimum: 6,
       message: 'must have at least 6 characters.',
