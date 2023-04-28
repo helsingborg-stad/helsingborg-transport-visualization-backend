66fe79aba929f0cc58522fdd105228152e0fec34
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-04-28 10:43:47 +0200

    update validation of password and pinCode

--- src/routes/auth/validation/login.validation.ts
+++ src/routes/auth/validation/login.validation.ts
@@ -6,15 +6,16 @@ export const loginValidation = validationMiddleware({
     type: 'string',
   },
   password: {
-    length: {
-      minimum: 10,
-      message: 'must have at least 10 characters.',
+    format: {
+      pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{10,}$/,
+      message:
+        'must be minimum 10 characters, must contain at least 1 special character, must contain at least 1 number, must contain at least 1 uppercase letter',
     },
   },
   pinCode: {
-    length: {
-      minimum: 6,
-      message: 'must have at least 6 characters.',
+    format: {
+      pattern: /^(?!.*(\d)\1{2})(?!.*(\d)(\d)\2{2})[0-9]{6}$/,
+      message: 'must be 6 digits, max 2 repeating after each other, min 3 unique digits',
     },
   },
 });
