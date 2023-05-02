6cc14df3df8f8cdc0b779caf2d90210caee745c4
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-02 15:27:30 +0200

    create zone service

---
+++ src/domains/zone/types.ts
@@ -0,0 +1,19 @@
+import { GeoJsonObject, Polygon } from 'geojson';
+import { ZoneType } from '@root/entities';
+
+export type ZoneCreateType = GeoJsonObject & {
+  type: 'FeatureCollection';
+  features: [
+    {
+      type: 'Feature';
+      geometry: Polygon;
+      properties: {
+        name: string;
+        address: string;
+        area: string;
+        type: ZoneType;
+        organisationId: string;
+      };
+    }
+  ];
+};
