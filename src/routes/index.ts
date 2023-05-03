ac63f575fb53dac5b40effd0e9f2df41b354c2bd
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-03 09:53:49 +0200

    remove legacy routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,8 @@
 import { Application } from 'express';
 import { authRoutes } from './auth';
-import { freightCompanyRoutes } from './freightCompany';
-import { userRoutes } from './user';
-import { geolocationRoutes } from './geolocation';
 import { zoneRoutes } from './zone';
 
 export const registerRoutes = (app: Application) => {
   app.use('/auth', authRoutes());
-  app.use('/freight-company', freightCompanyRoutes());
-  app.use('/user', userRoutes());
-  app.use('/geolocation', geolocationRoutes());
   app.use('/zones', zoneRoutes());
 };
