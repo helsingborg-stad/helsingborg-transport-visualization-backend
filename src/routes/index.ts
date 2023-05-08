7b416dd6682d2e8e46404f2e8d28bdc87bbbbbd7
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-08 11:07:24 +0200

    create get all orgs route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,11 @@ import { Application } from 'express';
 import { authRoutes } from './auth';
 import { zoneRoutes } from './zone';
 import { filterRoutes } from './filter';
+import { organisationRoutes } from './organisation';
+
 export const registerRoutes = (app: Application) => {
   app.use('/auth', authRoutes());
   app.use('/zones', zoneRoutes());
   app.use('/filters', filterRoutes());
+  app.use('/organisations', organisationRoutes());
 };
