6d9c94cdda0ac6aa4339a9439e775cf0248f7f9e
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-08 16:28:37 +0200

    implement post events route

--- src/routes/zone/zone.routes.ts
+++ src/routes/zone/zone.routes.ts
@@ -1,14 +1,16 @@
 import { Request, Response, Router } from 'express';
-import { createZonesValidation } from './validation';
+import { createZonesValidation, createEventValidation } from './validation';
 import { ZoneService, IZoneService } from '@domains/zone';
+import { IEventService, EventService } from '@root/domains/event';
 import { handleError } from '@root/utils/handleError';
 import { isAuth } from '@root/middlewares/isAuth';
 import { isPasswordAuthenticated } from '@root/middlewares/isPasswordAuthenticated';
-import { CreateZonesBody } from './types';
+import { CreateZonesBody, FilterEventQueryType, CreateEventBody, IdParamsType } from './types';
 
 export const zoneRoutes = () => {
   const router = Router();
   const zoneService: IZoneService = new ZoneService();
+  const eventService: IEventService = new EventService();
 
   /**
    * @swagger
@@ -71,5 +73,74 @@ export const zoneRoutes = () => {
     }
   });
 
+  /**
+   * @swagger
+   * /zones/events:
+   *  get:
+   *    summary: Get events
+   *    description: "Attempt to fetch events"
+   *    tags:
+   *      - Events
+   *      - Zones
+   *    consumes: application/json
+   *    responses:
+   *      200:
+   *        $ref: '#/components/responses/ListOfEvents'
+   */
+  router.get('/events', async (req: Request<null, null, null, FilterEventQueryType>, res: Response) => {
+    try {
+      const names = req.query.names?.split(',');
+      const orgNumbers = req.query.orgNumbers?.split(',');
+      const areas = req.query.areas?.split(',');
+      const results = await eventService.getEvents({
+        names,
+        orgNumbers,
+        areas,
+      });
+      res.status(200).send(results);
+    } catch (e) {
+      return handleError(e, res);
+    }
+  });
+
+  /**
+   * @swagger
+   * /zones/{id}/events:
+   *  post:
+   *    summary: Add zones to the database
+   *    description: "Attempt to create zones based on given FeatureCollection"
+   *    tags:
+   *      - Events
+   *      - Zones
+   *    consumes: application/json
+   *    requestBody:
+   *      content:
+   *        $ref: '#/components/requestBodies/Event'
+   *    responses:
+   *      201:
+   *        $ref: '#/components/responses/IEvent'
+   *      400:
+   *        $ref: '#/components/responses/BadRequestError'
+   *      401:
+   *        $ref: '#/components/responses/UnauthorizedError'
+   */
+  router.post(
+    '/zones/:id/events',
+    isAuth,
+    isPasswordAuthenticated(false),
+    createEventValidation,
+    async (req: Request<IdParamsType, null, CreateEventBody>, res: Response) => {
+      try {
+        //@ts-ignore
+        const { orgNumber } = req.auth;
+        const { id } = req.params;
+        const result = await eventService.createEvent(id, orgNumber, req.body);
+        res.status(201).send(result);
+      } catch (e) {
+        return handleError(e, res);
+      }
+    }
+  );
+  console.log('sadsasd');
   return router;
 };
