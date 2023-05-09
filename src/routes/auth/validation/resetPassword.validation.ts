37a25749dc305d8c7cfe666cb3bd75dd587a517f
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-09 15:07:57 +0200

    remove confirmedPassword from request

--- src/routes/auth/validation/resetPassword.validation.ts
+++ src/routes/auth/validation/resetPassword.validation.ts
@@ -10,9 +10,4 @@ export const resetPasswordValidation = validationMiddleware({
         'must be minimum 10 characters, must contain at least 1 special character, must contain at least 1 number, must contain at least 1 uppercase letter',
     },
   },
-  confirmedPassword: {
-    presence: true,
-    equality: 'password',
-    message: 'must match password',
-  },
 });
