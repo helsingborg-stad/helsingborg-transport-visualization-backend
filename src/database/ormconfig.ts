6a56de786cee971e0e17490bb8f408bdff7b3e22
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-11 11:22:38 +0200

    add host and port

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -22,8 +22,8 @@ const getDbSource = () => {
     case 'gcp':
       return {
         type: 'postgres',
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT as unknown as number,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as unknown as number,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
