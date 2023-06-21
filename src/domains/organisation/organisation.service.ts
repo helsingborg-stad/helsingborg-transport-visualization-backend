c10aa6d5fb6af1e394f7449ba505797eccda3c64
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-06-21 11:10:52 +0200

    add update logic in domain-layer

--- src/domains/organisation/organisation.service.ts
+++ src/domains/organisation/organisation.service.ts
@@ -1,10 +1,12 @@
 import { OrganisationResponse } from '@root/entities';
 import { OrganisationRepository, IOrganisationRepository, EventRepository, IEventRepository } from '@root/repositories';
+import { UpdateOrganisationBody } from './types';
 import StatusError from '@root/utils/statusError';
 
 export interface IOrganisationService {
   getAllOrganisations(): Promise<OrganisationResponse[]>;
-  deleteOrganisation(id: string, userId: string): Promise<OrganisationResponse>;
+  deleteOrganisation(id: string, userId: string): Promise<any>;
+  updateOrganisation(id: string, userId: string, body: UpdateOrganisationBody): Promise<OrganisationResponse>;
 }
 
 export class OrganisationService implements IOrganisationService {
@@ -17,11 +19,22 @@ export class OrganisationService implements IOrganisationService {
     return this.orgRepo.getAllOrganisations();
   }
 
-  async deleteOrganisation(id: string, userId: string): Promise<OrganisationResponse> {
+  async deleteOrganisation(id: string, userId: string): Promise<any> {
     if (id !== userId) throw new StatusError(401, 'Not authorized');
     const organisation = await this.orgRepo.findById(id);
     if (!organisation) throw new StatusError(404, 'Organisation not found');
     await this.eventRepo.deleteByOrgNumber(organisation.orgNumber);
     return this.orgRepo.deleteOrganisation(id);
+  }
+
+  async updateOrganisation(id: string, userId: string, body: UpdateOrganisationBody): Promise<OrganisationResponse> {
+    if (id !== userId) throw new StatusError(401, 'Not authorized');
+    const organisation = await this.orgRepo.findById(id);
+    if (!organisation) throw new StatusError(404, 'Organisation not found');
+    if (body.password) await organisation.setPassword(body.password);
+    if (body.pinCode) await organisation.setPinCode(body.pinCode);
+    if (body.contactPerson) organisation.contactPerson = body.contactPerson;
+    if (body.mobileNumber) organisation.mobileNumber = body.mobileNumber;
+    return this.orgRepo.save(organisation);
   }
 }
